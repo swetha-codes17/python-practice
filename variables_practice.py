61364397b203b5a272771af242f1ae3f7889f449
CHOWDABOINA SWETHA <chswetha017@gmail.com>
2026-06-04 13:12:43 +0530

    Added Variables Practice Program

---
+++ variables_practice.py
@@ -0,0 +1,14 @@
+name = "Swetha"
+branch = "CSE-DS"
+CGPA = "8.35"
+year = "4th Year"
+Favorite_Subject="Python"
+Dream_Company="Microsoft"
+
+print("===== STUDENT ID =====")
+print("Name:", name)
+print("Branch:", branch)
+print("CGPA:", CGPA)
+print("Year:", year)
+print("Favorite_Subject:",Favorite_Subject)
+print("Dream_Company:",Dream_Company)
